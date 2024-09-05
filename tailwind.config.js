f00cc6078320b48ab3a915e0016dd7d7ca6b1888
soummo <soummo.cloud@gmail.com>
2024-09-06 01:47:30 +0530

    updated on design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["*"],
   theme: {
     extend: {},
   },
   plugins: [],
-  fontFamily:{
-    Whitney:["Whitney"],
-    Ginto:["Ginto"],
-    ggSans:["ggSans"],
+  fontFamily: {
+    whitney: ['Whitney', 'sans-serif'],
+    ginto: ['Ginto', 'sans-serif'],
+    gintoNord: ['GintoNord', 'sans-serif'],
+    ggSans: ['"ggSans"', 'sans-serif'],
+  },
+  fontWeight: {
+    300: '300',
+    400: '400',
+    500: '500',
+    600: '600',
+    700: '700',
   },
 }
 
